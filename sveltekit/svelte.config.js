b645af5b731b2a7d5dc37af0143f6d4a4bcd74c6
Julien Sulpis <code@jsulpis.dev>
2025-12-11 21:44:37 +0100

    feat: add sveltekit app

---
+++ sveltekit/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+
+  kit: {
+    // Use adapter-static for SSG (static site generation)
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter(),
+  },
+};
+
+export default config;
